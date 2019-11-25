e1414e9f24387983cbf1aa13930effc5a7ca004d
Matias Navarro <matutenavarro95@gmail.com>
2019-11-25 20:45:21 -0300

    datapath

--- TP3-BIP1-2019/Verilog/datapath.v
+++ TP3-BIP1-2019/Verilog/datapath.v
@@ -22,10 +22,13 @@
 module datapath
 #(
     parameter   NB_DATA     = 15,
-                NB_OPERAND  = 11
+                NB_OPERAND  = 11,
+                NB_EXT      = 5
 )
 (
     //Inputs
+    input                       i_clk,
+    input                       i_reset,
     input   [1:0]               i_selA,
     input                       i_selB,
     input                       i_WrAcc,
@@ -36,5 +39,57 @@ module datapath
     output  [NB_OPERAND-1:0]    o_Addr,
     output  [NB_DATA-1:0]       o_data_memory
 );
+
+    reg [NB_DATA-1:0]    mux_a_out;
+    reg [NB_DATA-1:0]    mux_b_out;
+    reg [NB_DATA-1:0]    op_out;
+    reg [NB_DATA-1:0]    acc;   
+    reg [NB_DATA-1:0]    signal_extension;
+
+    //Signal Extension 
+    always @(*) begin
+        signal_extension = {{NB_EXT{i_Operand[NB_OPERAND-1]}},i_Operand}; //Extension del MSB del i_Operand
+    end
+
+    //mux_A
+    always @(*)begin
+        case(i_selA)
+            2'b00:     mux_a_out = i_data_memory;
+            2'b01:     mux_a_out = signal_extension;
+            2'b10:     mux_a_out = op_out;
+            default:   mux_a_out = 0; 
+        endcase
+    end
+
+    //mux_B
+    always @(*) begin
+        case(i_selB)
+            1'b0:   i_data_memory;
+            1'b1:   signal_extension;
+        endcase
+    end
+
+    //ACC
+    always @(posedge i_clk) begin
+        if(!i_rst)
+            acc <= {(NB_DATA-1){1'b0}};
+        else begin
+            if(i_WrAcc)
+                acc <= mux_a_out;
+            else
+                acc <= acc;
+        end
+    end
+
+    //Operacion
+    always @(*) begin
+        case(i_Op)
+            1'b0:   op_out = acc - mux_b_out;
+            1'b1:   op_out = acc + mux_b_out;
+        endcase
+    end
+
+    assign o_Addr           = i_Operand;
+    assign o_data_memory    = acc; 
 
 endmodule
