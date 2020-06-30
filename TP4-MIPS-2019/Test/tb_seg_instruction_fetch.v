44580794d7748c337fd44062ec823ec6e6a2b0a7
Matias Navarro <matutenavarro95@gmail.com>
2020-06-30 19:32:14 -0300

    Tests

--- TP4-MIPS-2019/Test/tb_seg_instruction_fetch.v
+++ TP4-MIPS-2019/Test/tb_seg_instruction_fetch.v
@@ -1,46 +1,55 @@
 `timescale 1ns / 1ps
 
+
+//Todo puerto de salida del modulo es un cable.
+//Todo puerto de estimulo o generacion de entrada es un registro.
 module tb_seg_instruction_fetch();
 
-	parameter NB_INSTRUC      = 32;
-    parameter NB_ADDR         = 32;
+	parameter LEN             = 32;
     parameter NB_DATA         = 16;
     //PROGRAM MEMORY
     parameter RAM_WIDTH_PROGRAM       = 32;
     parameter RAM_DEPTH_PROGRAM       = 2048;
     parameter RAM_PERFORMANCE_PROGRAM = "LOW_LATENCY";
-    parameter INIT_FILE_PROGRAM       = "C:\\Users\\astar\\git\\Arquitectura_de_Computadoras_2019\\TP4-MIPS-2019\\Test\\init_mem_instruction_fetch.mem";
+    parameter INIT_FILE_PROGRAM       = "init_mem_instruction_fetch.mem";
     
     // INPUTS
-    reg                          i_clk;
-    reg                          i_rst;
-    reg [NB_ADDR - 1 : 0]        i_PC_branch;
-    reg                          i_PCSrc;
+    reg                 i_clk;
+    reg                 i_rst;
+    //reg [LEN - 1 : 0]   i_PC_add;       //PC+4
+    reg [LEN - 1 : 0]   i_PC_branch;    //PC Branch(Jump) 
+    reg                 i_PCSrc;
     // OUTPUTS
-    wire [NB_INSTRUC - 1 : 0]    o_instruction;
-    wire [NB_ADDR - 1 : 0]       o_PC;
+    wire [LEN - 1 : 0]  o_instruction;
+    wire [LEN - 1 : 0]  o_PC;
 
     initial begin
-        i_clk = 1'b0;
-        i_rst = 1'b0;
-        i_PC_branch = {NB_INSTRUC{1'b0}};
+        i_clk       = 1'b0;
+        i_rst       = 1'b0;
+        //i_PC_add    = {LEN{1'b0}};
+        i_PC_branch = {LEN{1'b0}};
         i_PCSrc = 1'b0;
         
-        #10 i_rst = 1'b1;
-
-        #30
-        i_PCSrc = 1'b1;
-        i_PC_branch = 3;
+        #10 
+        i_rst = 1'b1;
 
         #10
-        i_PCSrc = 1'b0;
+        i_PCSrc     = 1'b1;
+        //i_PC_add    = 1;
         i_PC_branch = 2;
 
-        #10 i_PC_branch = 10;
         #10
-        i_PC_branch = 0;
-        i_PCSrc = 1'b1;
-        #2 i_PCSrc = 1'b0;
+        i_PCSrc     = 1'b0;
+        //i_PC_add    = 1;
+        i_PC_branch = 2;
+
+//        #10 i_PC_branch = 10;
+        
+//        #10
+//        i_PC_branch = 0;
+//        i_PCSrc = 1'b1;
+        
+//        #2 i_PCSrc = 1'b0;
         
         #10 $finish;
     end
@@ -48,9 +57,7 @@ module tb_seg_instruction_fetch();
     always #1 i_clk = ~i_clk;
 
     seg_instruction_fetch #(
-        .NB_INSTRUC (NB_INSTRUC),
-        .NB_ADDR    (NB_ADDR),
-        .NB_DATA    (NB_DATA),
+        .LEN                        (LEN),
         .RAM_WIDTH_PROGRAM 			(RAM_WIDTH_PROGRAM),
         .RAM_DEPTH_PROGRAM 			(RAM_DEPTH_PROGRAM),
         .RAM_PERFORMANCE_PROGRAM 	(RAM_PERFORMANCE_PROGRAM),
@@ -61,6 +68,7 @@ module tb_seg_instruction_fetch();
         //Input
         .i_clk              (i_clk),
         .i_rst              (i_rst),
+        //.i_PC_add           (i_PC_add),
         .i_PC_branch        (i_PC_branch),
         .i_PCSrc            (i_PCSrc),
         //Output
