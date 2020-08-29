ee2e44d1cd7d5cc37ef10bdb38f6c9c8585a8672
Matias Navarro <matutenavarro95@gmail.com>
2020-08-29 16:04:55 -0300

    Correccion archivos, del push anterior

--- TP4-MIPS-2019/Test/tb_seg_instruction_decode.v
+++ TP4-MIPS-2019/Test/tb_seg_instruction_decode.v
@@ -1,230 +1,185 @@
 `timescale 1ns / 1ps
 
-module control
-    #(
-        parameter NB_OPCODE     = 6,
-        parameter NB_CTRL_EX    = 6,
-        parameter NB_CTRL_M     = 9,
-        parameter NB_CTRL_WB    = 2
-     )
-    (
-        // Inputs
-        input                           i_rst,
-        input       [NB_OPCODE-1:0]     i_opcode,
-        input       [NB_OPCODE-1:0]     i_funct,
-        // Outputs (organiza las salidas en buses, para mayor prolijidad)
-        output reg  [NB_CTRL_WB-1:0]    o_ctrl_wb_bus,      // [ RegWrite, MemtoReg]
-        output reg  [NB_CTRL_M-1:0]     o_ctrl_mem_bus,     // [ SB, SH, LB, LH, Unsigned, BNEQ, Branch, MemRead, MemWrite ]
-        output reg  [NB_CTRL_EX-1:0]    o_ctrl_exc_bus,     // [ ALUSrc, AluOp[3], AluOp[2], AluOp[1], AluOp[0], RegDst]
-        output reg                      o_Jump,
-        output reg                      o_JAL,
-        output reg                      o_JR,
-        output reg                      o_JALR
-    );
+//Todo puerto de salida del modulo es un cable.
+//Todo puerto de estimulo o generacion de entrada es un registro.
+module tb_seg_instruction_decode();
+    localparam LEN           = 32;
+    localparam NB_REG        = 32;
+    localparam NB_ADDRESS    = 16;
+    localparam NB_OPCODE     = 6;
+    localparam NB_ADDR       = 5;
+    localparam NB_CTRL_EX    = 6;
+    localparam NB_CTRL_M     = 9;
+    localparam NB_CTRL_WB    = 2;
+
+    //Entradas 
+    reg                     i_clk;
+    reg                     i_rst;
+    reg [LEN-1:0]           i_PC;
+    reg [LEN-1:0]           i_instruction;
+    reg [NB_ADDR-1:0]       i_write_reg;
+    reg [LEN-1:0]           i_write_data;
+    reg                     i_RegWrite;
+    reg                     i_flush;
+    reg [NB_ADDR-1:0]       i_rt_ex;       
+//    reg                     i_PCSrc;
+//    reg                     i_stall_flag;
     
-    always@(*) begin
-        o_Jump  = 0;
-        o_JAL   = 0;
-        o_JR    = 0;
-        o_JALR  = 0;
-        if(!i_rst) begin
-            o_ctrl_wb_bus   = 0;
-            o_ctrl_mem_bus  = 0;
-            o_ctrl_exc_bus  = 0;
-        end 
-        else begin
-            o_ctrl_wb_bus   = o_ctrl_wb_bus;
-            o_ctrl_mem_bus  = o_ctrl_mem_bus;
-            o_ctrl_exc_bus  = o_ctrl_exc_bus;
+    //Salidas
+    wire [NB_ADDR-1:0]      o_rs;           //instruction[25:21]
+    wire [NB_ADDR-1:0]      o_rt;           //instruction[20:16]
+    wire [NB_ADDR-1:0]      o_rd;           //instruction[15:11]    
+    wire [LEN-1:0]          o_PC;
+    wire [LEN-1:0]          o_addr_ext;
+    wire [LEN-1:0]          o_read_data_1;
+    wire [LEN-1:0]          o_read_data_2;
+    wire [LEN-1:0]          o_PC_dir_jump;
+    wire                    o_jump_flag;
+    wire                    o_stall_flag;
+//    wire                    o_flush_if;     
+//    wire                    o_flush_ex; 
+    
+    //Control outputs 
+    wire [NB_CTRL_WB-1:0]   o_ctrl_wb_bus;
+    wire [NB_CTRL_M-1:0]    o_ctrl_mem_bus;
+    wire [NB_CTRL_EX-1:0]   o_ctrl_exc_bus;
 
-            case(i_opcode)
-                //R-TYPE ------------------------------------------
-                6'b 000000: 
-                begin
-                    case(i_funct)
-                        6'b000000, 6'b000010, 6'b000011: //SHIFT CON SHAMT (SLL - SRL - SRA)
-                        begin
-                            o_ctrl_wb_bus    = 2'b10;
-                            o_ctrl_mem_bus   = 9'b000000000;
-                            o_ctrl_exc_bus   = 6'b100101;
-                        end
+    initial begin
+        i_clk = 1'b0;
+        i_rst = 1'b0;
+        i_write_data    = 32'h0000abcd;
+        i_PC[31:26]     = 6'b111100; i_PC[25:0] = $random; 
+        i_instruction   = 32'b00000000;
+        i_write_reg     = 5'b00010;
+        i_rt_ex         = 5'b00001;
+        i_RegWrite      = 1'b0;
+        i_flush         = 1'b1;
 
-                        6'b001000: //o_JR
-                        begin
-                            o_ctrl_wb_bus    = 2'b10;
-                            o_ctrl_mem_bus   = 9'b000000000;
-                            o_ctrl_exc_bus   = 6'b000000;
-                            o_JR             = 1;
-                        end
+        #5 i_rst    = 1'b1;
+           i_flush  = 1'b0;
+        
+        //R-TYPE ---------------------------------------------
+        #5 i_instruction [31:26]   = 6'b000000;        //R-Type
+            i_instruction [25:6 ]   = $random;
+            i_instruction [5 :0 ]   = 6'b100001;        //ADDU 
+            i_RegWrite              = 1'b1;        
+        
+        #5  i_instruction [5 :0 ]   = 6'b100100;        //AND 
+        #5  i_instruction [5 :0 ]   = 6'b100101;        //OR
+  
+        #5  i_instruction [5 :0 ]   = 6'b001000;        //JR
+        #5  i_instruction [5 :0 ]   = 6'b001001;        //JALR
 
-                        6'b001001: //o_JALR
-                        begin
-                            o_ctrl_wb_bus    = 2'b10;
-                            o_ctrl_mem_bus   = 9'b000000000;
-                            o_ctrl_exc_bus   = 6'b000001;
-                            o_JALR           = 1;
-                        end  
+       //LOAD TYPE ------------------------------------------
+       
+       #5  i_instruction [31:26]   = 6'b100000;        //LB
+           i_instruction [25:21]   = 5'b01011;
+           i_instruction [20:16]   = 5'b00001;
+           i_instruction [15:0 ]   = $random;
+           i_RegWrite              = 1'b1;
+           
 
-                        default:
-                        begin
-                            o_ctrl_wb_bus    = 2'b10;
-                            o_ctrl_mem_bus   = 9'b000000000;
-                            o_ctrl_exc_bus   = 6'b000101;
-                        end 
-                    endcase
-                end
+       #5  i_instruction [31:26]   = 6'b100001;        //LH
+           i_instruction [20:16]   = 5'b00010;
+           i_RegWrite              = 1'b1;
+        
+       #5  i_instruction [31:26]   = 6'b100011;        //LW
+           i_instruction [25:0 ]   = $random;
+           i_RegWrite              = 1'b1;
 
-                //LOAD TYPE ---------------------------------------
+       #5  i_instruction [31:26]   = 6'b100111;        //LWU
+           i_instruction [25:0 ]   = $random;
+           i_RegWrite              = 1'b1;
+        
+       #5  i_instruction [31:26]   = 6'b100100;        //LBU
+           i_instruction [25:0 ]   = $random;
+           i_RegWrite              = 1'b1;
+        
+       #5  i_instruction [31:26]   = 6'b100101;        //LHU
+           i_instruction [25:0 ]   = $random;
+           i_RegWrite              = 1'b1;
 
-                6'b 100000: //LB
-                begin
-                    o_ctrl_wb_bus    = 2'b11;
-                    o_ctrl_mem_bus   = 9'b001000010;
-                    o_ctrl_exc_bus   = 6'b100000;
-                end
+        //STORE TYPE -----------------------------------------
+        #5  i_instruction [31:26]   = 6'b101000;        //SB
+            i_instruction [25:0 ]   = $random;
+            i_RegWrite              = 1'b0;
 
-                6'b 100001: //LH
-                begin
-                    o_ctrl_wb_bus    = 2'b11;
-                    o_ctrl_mem_bus   = 9'b000100010;
-                    o_ctrl_exc_bus   = 6'b100000;
-                end
+        #5  i_instruction [31:26]   = 6'b101001;        //SH
+            i_instruction [25:0 ]   = $random;
+            i_RegWrite              = 1'b0;
 
-                6'b 100011: //LW
-                begin
-                    o_ctrl_wb_bus    = 2'b11;
-                    o_ctrl_mem_bus   = 9'b000000010;
-                    o_ctrl_exc_bus   = 6'b100000;
-                end
+        #5  i_instruction [31:26]   = 6'b101011;        //SW
+            i_instruction [25:0 ]   = $random;
+            i_RegWrite              = 1'b0;
 
-                6'b 100111: //LWU
-                begin
-                    o_ctrl_wb_bus    = 2'b11;
-                    o_ctrl_mem_bus   = 9'b000000010;
-                    o_ctrl_exc_bus   = 6'b100000;
-                end
+        //INMEDIATE -------------------------------------------
+        #5  i_instruction [31:26]   = 6'b001000;        //ADDI
+            i_instruction [25:0 ]   = $random;
+            i_RegWrite              = 1'b1;
 
-                6'b 100100: //LBU
-                begin
-                    o_ctrl_wb_bus    = 2'b11;
-                    o_ctrl_mem_bus   = 9'b001010010;
-                    o_ctrl_exc_bus   = 6'b100000;
-                end
+        //BRANCH - JUMP ---------------------------------------
+        #5  i_instruction [31:26]   = 6'b000100;        //BEQ
+            i_instruction [25:0 ]   = $random;
+            i_RegWrite              = 1'b0;
 
-                6'b 100101: //LHU
-                begin
-                    o_ctrl_wb_bus    = 2'b11;
-                    o_ctrl_mem_bus   = 9'b000110010;
-                    o_ctrl_exc_bus   = 6'b100000;
-                end
+        #5  i_instruction [31:26]   = 6'b000101;        //BNQ
+            i_instruction [25:0 ]   = $random;
+            i_RegWrite              = 1'b0;
 
-                //STORE TYPE ---------------------------------------
-                                
-                6'b 101000: //SB
-                begin
-                    o_ctrl_wb_bus    = 2'b00;
-                    o_ctrl_mem_bus   = 9'b100000001;
-                    o_ctrl_exc_bus   = 6'b100000;
-                end
+        #5  i_instruction [31:26]   = 6'b000010;        //JUMP
+            i_instruction [25:0 ]   = $random;
+            i_RegWrite              = 1'b0;
 
-                6'b 101001: //SH
-                begin
-                    o_ctrl_wb_bus    = 2'b00;
-                    o_ctrl_mem_bus   = 9'b010000001;
-                    o_ctrl_exc_bus   = 6'b100000;
-                end
+        #5  i_instruction [31:26]   = 6'b000011;        //JAL
+            i_instruction [25:0 ]   = $random;
+            i_RegWrite              = 1'b0;
 
-                6'b 101011: //SW
-                begin
-                    o_ctrl_wb_bus    = 2'b00;
-                    o_ctrl_mem_bus   = 9'b000000001;
-                    o_ctrl_exc_bus   = 6'b100000;
-                end
 
-                //INMEDIATE ---------------------------------------
-
-                6'b 001000: //ADDI
-                begin
-                    o_ctrl_wb_bus    = 2'b10;
-                    o_ctrl_mem_bus   = 9'b000000000;
-                    o_ctrl_exc_bus   = 6'b100110;
-                end
-
-                6'b 001100: //ANDI
-                begin
-                    o_ctrl_wb_bus    = 2'b10;
-                    o_ctrl_mem_bus   = 9'b000000000;
-                    o_ctrl_exc_bus   = 6'b101000;
-                end
-
-                6'b 001101: //ORI
-                begin
-                    o_ctrl_wb_bus    = 2'b10;
-                    o_ctrl_mem_bus   = 9'b000000000;
-                    o_ctrl_exc_bus   = 6'b101010;
-                end
-
-                6'b 001110: //XORI
-                begin
-                    o_ctrl_wb_bus    = 2'b10;
-                    o_ctrl_mem_bus   = 9'b000000000;
-                    o_ctrl_exc_bus   = 6'b101100;
-                end
-
-                6'b 001111: //LUI
-                begin
-                    o_ctrl_wb_bus    = 2'b10;
-                    o_ctrl_mem_bus   = 9'b000000000;
-                    o_ctrl_exc_bus   = 6'b101110;
-                end
-
-                6'b 001010: //SLTI
-                begin
-                    o_ctrl_wb_bus    = 2'b10;
-                    o_ctrl_mem_bus   = 9'b000000000;
-                    o_ctrl_exc_bus   = 6'b110000;
-                end
-
-                //BRANCH - o_Jump ---------------------------------------
-                                
-                6'b 000100: //BEQ
-                begin
-                    o_ctrl_wb_bus    = 2'b00;
-                    o_ctrl_mem_bus   = 9'b000000010;
-                    o_ctrl_exc_bus   = 6'b100010;
-                end
-                
-                6'b 000101: //BNQ
-                begin
-                    o_ctrl_wb_bus    = 2'b00;
-                    o_ctrl_mem_bus   = 9'b000001100;
-                    o_ctrl_exc_bus   = 6'b100010;
-                end
-
-                6'b 000010: //o_Jump (Salto incondicional) 
-                begin
-                    o_ctrl_wb_bus    = 2'b00;
-                    o_ctrl_mem_bus   = 9'b000000000;
-                    o_ctrl_exc_bus   = 6'b000000;
-                    o_Jump           = 1;
-                end
-
-                6'b 000011: //o_JAL
-                begin
-                    o_ctrl_wb_bus    = 2'b10;   
-                    o_ctrl_mem_bus   = 9'b000000000;
-                    o_ctrl_exc_bus   = 6'b000000;
-                    o_JAL            = 1;
-                end
-                
-                default:
-                begin
-                    o_ctrl_wb_bus    = 2'b00;
-                    o_ctrl_mem_bus   = 9'b000000000;
-                    o_ctrl_exc_bus   = 6'b000000;
-                end
-            endcase
-        end
+        #5  i_instruction           = 32'h01094020;     //R-Type
+                    
+        #5 $finish;
     end
+
+    always #2.5 i_clk = ~i_clk;
+
+    seg_instruction_decode 
+    #(
+        .LEN        (LEN        ),
+        .NB_REG     (NB_REG     ),
+        .NB_ADDRESS (NB_ADDRESS ),
+        .NB_OPCODE  (NB_OPCODE  ),
+        .NB_ADDR    (NB_ADDR    ),
+        .NB_CTRL_EX (NB_CTRL_EX ),
+        .NB_CTRL_M  (NB_CTRL_M  ),
+        .NB_CTRL_WB (NB_CTRL_WB )
+    )
+    u_seg_instruction_decode(
+    	.i_clk          (i_clk          ),
+        .i_rst          (i_rst          ),
+        .i_PC           (i_PC           ),
+        .i_instruction  (i_instruction  ),
+        .i_write_reg    (i_write_reg    ),
+        .i_write_data   (i_write_data   ),
+        .i_RegWrite     (i_RegWrite     ),
+        .i_flush        (i_flush        ),
+        .i_rt_ex        (i_rt_ex        ),
+//        .i_PCSrc        (i_PCSrc        ),
+//        .i_stall_flag   (i_stall_flag   ),
+        .o_rs           (o_rs           ),
+        .o_rt           (o_rt           ),
+        .o_rd           (o_rd           ),
+        .o_PC           (o_PC           ),
+        .o_addr_ext     (o_addr_ext     ),
+        .o_read_data_1  (o_read_data_1  ),
+        .o_read_data_2  (o_read_data_2  ),
+        .o_PC_dir_jump  (o_PC_dir_jump  ),
+        .o_jump_flag    (o_jump_flag    ),
+        .o_stall_flag   (o_stall_flag   ),
+//        .o_flush_if     (o_flush_if     ),
+//        .o_flush_ex     (o_flush_ex     ),
+        .o_ctrl_wb_bus  (o_ctrl_wb_bus  ),
+        .o_ctrl_mem_bus (o_ctrl_mem_bus ),
+        .o_ctrl_exc_bus (o_ctrl_exc_bus )
+    );
 
 endmodule
